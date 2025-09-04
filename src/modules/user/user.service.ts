3a22af55110fb6cd52f72d5dc52aa452a9673a4e
mohamedsalah50 <mohamedamer2003250@gmail.com>
2025-09-05 00:37:37 +0300

    presigned url , delete folders by prefix

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -2,69 +2,109 @@ import { Request, Response } from "express";
 // import { ILogoutDto } from "./user.dto";
 import { UpdateQuery } from "mongoose";
 import { HUserDocument, IUser, UserModel } from "../../db/models/user.model";
-import { createLoginCredentials, createRevokeToken, LogOutEnum } from "../../utils/security/token.security";
+import {
+  createLoginCredentials,
+  createRevokeToken,
+  LogOutEnum,
+} from "../../utils/security/token.security";
 import { userRepository } from "../../db/repository/user.repository";
 // import { TokenRepository } from "../../db/repository/token.repository";
 // import { TokenModel } from "../../db/models/token.model";
 // import { ILogoutDto } from "./user.dto";
 import { JwtPayload } from "jsonwebtoken";
-import { uploadFile } from "../../utils/multer/s3.config";
+import {
+  createSignedUploadLink,
+  uploadFiles,
+} from "../../utils/multer/s3.config";
+import { storageEnum } from "../../utils/multer/cloud.multer";
 
 class UserService {
-    private userModel = new userRepository(UserModel);
-    // private tokenModel = new TokenRepository(TokenModel);
-    constructor() { }
-    profile = async (req: Request, res: Response): Promise<Response> => {
-        return res.json({
-            message: "user profile", data: {
-                user: req.user?._id,
-                decoded: req.decoded?.iat
-            }
-        })
-    }
+  private userModel = new userRepository(UserModel);
+  // private tokenModel = new TokenRepository(TokenModel);
+  constructor() {}
+  profile = async (req: Request, res: Response): Promise<Response> => {
+    return res.json({
+      message: "user profile",
+      data: {
+        user: req.user?._id,
+        decoded: req.decoded?.iat,
+      },
+    });
+  };
 
+  profileImage = async (req: Request, res: Response): Promise<Response> => {
+    const {
+      contentType,
+      originalname,
+    }: { contentType: string; originalname: string } = req.body;
+    const { url, key } = await createSignedUploadLink({
+      path: `users/${req.decoded?._id}`,
+      ContentType: contentType,
+      OriginalName: originalname,
+    });
+    return res.json({
+      message: "profile-image",
+      data: {
+        url,
+        key,
+      },
+    });
+    // const key = await uploadFile({
+    //   file: req.file as Express.Multer.File,
+    //   path: `users/${req.decoded?._id}`,
+    // });
+    // return res.json({
+    //   message: "profile-image",
+    //   data: {
+    //     key,
+    //   },
+    // });
+  };
 
-    profileImage = async (req: Request, res: Response): Promise<Response> => {
-        const key = await uploadFile({ file: req.file as Express.Multer.File, path: `users/${req.decoded?._id}` })
-        return res.json({
-            message: "profile-image", data: {
-                key
-            }
-        })
-    }
+  profileCoverImage = async (
+    req: Request,
+    res: Response
+  ): Promise<Response> => {
+    const urls = await uploadFiles({
+      storageAppraoch: storageEnum.disk,
+      files: req.files as Express.Multer.File[],
+      path: `users/${req.decoded?._id}/cover`,
+    });
+    return res.json({
+      message: "profile-image",
+      data: {
+        urls,
+      },
+    });
+  };
 
+  logout = async (req: Request, res: Response): Promise<Response> => {
+    const flag: LogOutEnum = req.body;
+    let statusCode: number = 200;
+    const update: UpdateQuery<IUser> = {};
 
-    logout = async (req: Request, res: Response): Promise<Response> => {
-        const flag: LogOutEnum = req.body;
-        let statusCode: number = 200
-        const update: UpdateQuery<IUser> = {};
-
-        switch (flag) {
-            case LogOutEnum.all:
-                update.changeCredentialsTime = new Date();
-                break;
-            default:
-                await createRevokeToken(req.decoded as JwtPayload);
-                statusCode = 201
-                break;
-        }
-
-
-
-        await this.userModel.updateOne({ filter: { _id: req.user?._id }, update });
-
-        return res.status(statusCode).json({
-            message: "user profile"
-        })
-    }
-
-    refreshToken = async (req: Request, res: Response): Promise<Response> => {
-        const credentials = await createLoginCredentials(req.user as HUserDocument);
+    switch (flag) {
+      case LogOutEnum.all:
+        update.changeCredentialsTime = new Date();
+        break;
+      default:
         await createRevokeToken(req.decoded as JwtPayload);
-        return res.status(201).json({ message: "done", data: { credentials } })
+        statusCode = 201;
+        break;
     }
 
-}
+    await this.userModel.updateOne({ filter: { _id: req.user?._id }, update });
 
+    return res.status(statusCode).json({
+      message: "user profile",
+    });
+  };
+
+  refreshToken = async (req: Request, res: Response): Promise<Response> => {
+    const credentials = await createLoginCredentials(req.user as HUserDocument);
+    await createRevokeToken(req.decoded as JwtPayload);
+    return res.status(201).json({ message: "done", data: { credentials } });
+  };
+}
 
 export default new UserService();
--- src/utils/multer/s3.config.ts
+++ src/utils/multer/s3.config.ts
@@ -1,36 +1,319 @@
-import { ObjectCannedACL, PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
-import { v4 as uuid } from "uuid"
+import {
+  DeleteObjectCommand,
+  DeleteObjectsCommand,
+  DeleteObjectsCommandOutput,
+  GetObjectCommand,
+  GetObjectCommandOutput,
+  ListObjectsCommand,
+  ListObjectsCommandOutput,
+  ObjectCannedACL,
+  PutObjectCommand,
+  S3Client,
+} from "@aws-sdk/client-s3";
+import { v4 as uuid } from "uuid";
 import { storageEnum } from "./cloud.multer";
 import { createReadStream } from "node:fs";
 import { BadRequest } from "../response/error.response";
+import { Upload } from "@aws-sdk/lib-storage";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 export const s3config = () => {
-    return new S3Client({
-        region: process.env.S3_REGION as string,
-        credentials: {
-            accessKeyId: process.env.S3_ACCESS_KEY as string,
-            secretAccessKey: process.env.S3_SECRET_KEY as string,
-        }
-    })
-}
+  return new S3Client({
+    region: process.env.S3_REGION as string,
+    credentials: {
+      accessKeyId: process.env.S3_ACCESS_KEY as string,
+      secretAccessKey: process.env.S3_SECRET_KEY as string,
+    },
+  });
+};
 
+export const uploadFile = async ({
+  storageAppraoch = storageEnum.memory,
+  Bucket = process.env.S3_BUCKET_NAME,
+  ACL = "private",
+  path = "general",
+  file,
+}: {
+  storageAppraoch?: storageEnum;
+  Bucket?: string;
+  ACL?: ObjectCannedACL;
+  path?: string;
+  file: Express.Multer.File;
+}): Promise<string> => {
+  const command = new PutObjectCommand({
+    Bucket,
+    ACL,
+    Key: `${process.env.APPLICATION_NAME}/${path}/${uuid()}/_${
+      file.originalname
+    }` as string,
+    Body:
+      storageAppraoch === storageEnum.memory
+        ? file.buffer
+        : createReadStream(file.path),
+    ContentType: file.mimetype,
+  });
 
-export const uploadFile = async ({ storageAppraoch = storageEnum.memory, Bucket = process.env.S3_BUCKET_NAME, ACL = "private", path = "general", file }:
-    { storageAppraoch?: storageEnum, Bucket?: string, ACL?: ObjectCannedACL, path?: string, file: Express.Multer.File }): Promise<string> => {
-    const command = new PutObjectCommand({
-        Bucket,
-        ACL,
-        Key: `${process.env.APPLICATION_NAME}/${path}/${uuid()}/_${file.originalname}` as string,
-        Body: storageAppraoch === storageEnum.memory ? file.buffer : createReadStream(file.path),
-        ContentType: file.mimetype
-    })
+  await s3config().send(command);
 
-    await s3config().send(command)
+  if (!command?.input?.Key) {
+    throw new BadRequest("fail to upload this file");
+  }
 
-    if (!command?.input?.Key) {
-        throw new BadRequest("fail to upload this file")
-    }
+  return command.input.Key;
+};
 
-    return command.input.Key
+export const uploadLargeFile = async ({
+  storageAppraoch = storageEnum.memory,
+  Bucket = process.env.S3_BUCKET_NAME,
+  ACL = "private",
+  path = "general",
+  file,
+}: {
+  storageAppraoch?: storageEnum;
+  Bucket?: string;
+  ACL?: ObjectCannedACL;
+  path?: string;
+  file: Express.Multer.File;
+}): Promise<string> => {
+  const upload = new Upload({
+    client: s3config(),
+    params: {
+      Bucket,
+      ACL,
+      Key: `${process.env.APPLICATION_NAME}/${path}/${uuid()}/_${
+        file.originalname
+      }` as string,
+      Body:
+        storageAppraoch === storageEnum.memory
+          ? file.buffer
+          : createReadStream(file.path),
+      ContentType: file.mimetype,
+    },
+  });
+  upload.on("httpUploadProgress", (progress) => {
+    console.log("upload file progress is ", progress);
+  });
 
-}
+  const { Key } = await upload.done();
+  if (!Key) {
+    throw new BadRequest("fail to generate this key");
+  }
+  return Key;
+};
+
+export const uploadFiles = async ({
+  storageAppraoch = storageEnum.memory,
+  Bucket = process.env.S3_BUCKET_NAME as string,
+  ACL = "private",
+  path = "general",
+  files,
+  useLarge = false,
+}: {
+  storageAppraoch?: storageEnum;
+  Bucket?: string;
+  ACL?: ObjectCannedACL;
+  path?: string;
+  files: Express.Multer.File[];
+  useLarge?: boolean;
+}): Promise<string[]> => {
+  let urls: string[] = [];
+
+  if (useLarge) {
+    urls = await Promise.all(
+      files.map((file) => {
+        return uploadLargeFile({
+          storageAppraoch,
+          Bucket,
+          ACL,
+          path,
+          file,
+        });
+      })
+    );
+  } else {
+    urls = await Promise.all(
+      files.map((file) => {
+        return uploadFile({
+          storageAppraoch,
+          Bucket,
+          ACL,
+          path,
+          file,
+        });
+      })
+    );
+  }
+
+  return urls;
+};
+
+// export const uploadLargeFiles = async ({
+//   storageAppraoch = storageEnum.disk,
+//   Bucket = process.env.S3_BUCKET_NAME as string,
+//   ACL = "private",
+//   path = "general",
+//   files,
+// }: {
+//   storageAppraoch?: storageEnum;
+//   Bucket?: string;
+//   ACL?: ObjectCannedACL;
+//   path?: string;
+//   files: Express.Multer.File[];
+// }): Promise<string[]> => {
+//   let urls: string[] = [];
+
+//   urls = await Promise.all(
+//     files.map((file) => {
+//       return uploadLargeFile({
+//         storageAppraoch,
+//         Bucket,
+//         ACL,
+//         path,
+//         file,
+//       });
+//     })
+//   );
+
+//   return urls;
+// };
+
+export const createSignedUploadLink = async ({
+  Bucket = process.env.S3_BUCKET_NAME as string,
+  path = "general",
+  expiresIn = 120,
+  ContentType,
+  OriginalName,
+}: {
+  Bucket?: string;
+  path?: string;
+  expiresIn?: number;
+  ContentType: string;
+  OriginalName: string;
+}): Promise<{ url: string; key: string }> => {
+  const command = new PutObjectCommand({
+    Bucket,
+    Key: `${process.env.APPLICATION_NAME}/${path}/${uuid()}/_${OriginalName}`,
+    ContentType,
+  });
+  const url = await getSignedUrl(s3config(), command, { expiresIn });
+
+  if (!url || !command?.input?.Key) {
+    throw new BadRequest("fail to create preSignedUrl");
+  }
+  return { url, key: command.input.Key };
+};
+
+export const createGetSignedLink = async ({
+  Bucket = process.env.S3_BUCKET_NAME as string,
+  expiresIn = 120,
+  Key,
+  downloadName = "dummy",
+  download = "flase",
+}: {
+  Bucket?: string;
+  expiresIn?: number;
+  Key: string;
+  downloadName?: string;
+  download?: string;
+}): Promise<string> => {
+  const command = new GetObjectCommand({
+    Key,
+    Bucket,
+    ResponseContentDisposition:
+      download === "true"
+        ? `attachment; filename=${downloadName || Key.split("/").pop()} `
+        : undefined,
+  });
+  const url = await getSignedUrl(s3config(), command, { expiresIn });
+
+  if (!url) {
+    throw new BadRequest("fail to create this upload preSignedUrl");
+  }
+  return url;
+};
+
+export const getFile = async ({
+  Bucket = process.env.S3_BUCKET_NAME as string,
+  key,
+}: {
+  Bucket?: string;
+  key: string;
+}): Promise<GetObjectCommandOutput> => {
+  const command = new GetObjectCommand({
+    Key: key,
+    Bucket,
+  });
+  return await s3config().send(command);
+};
+
+export const deleteFile = async ({
+  Key,
+  Bucket = process.env.S3_BUCKET_NAME,
+}: {
+  Key: string;
+  Bucket?: string;
+}) => {
+  const command = new DeleteObjectCommand({
+    Key,
+    Bucket,
+  });
+  return await s3config().send(command);
+};
+
+export const deleteFiles = async ({
+  Bucket = process.env.S3_BUCKET_NAME,
+  urls,
+  Quiet = false,
+}: {
+  Bucket?: string;
+  urls: string[];
+  Quiet?: boolean;
+}): Promise<DeleteObjectsCommandOutput> => {
+  const Objects = urls.map((url) => {
+    return { Key: url };
+  });
+  const command = new DeleteObjectsCommand({
+    Bucket,
+    Delete: {
+      Objects,
+      Quiet,
+    },
+  });
+  return await s3config().send(command);
+};
+
+export const listDirectoryFiles = async ({
+  Bucket = process.env.S3_BUCKET_NAME as string,
+  path,
+}: {
+  Bucket?: string;
+  path: string;
+}): Promise<ListObjectsCommandOutput> => {
+  const command = new ListObjectsCommand({
+    Bucket,
+    Prefix: `${process.env.APPLICATION_NAME}/${path}`,
+  });
+  return await s3config().send(command);
+};
+
+export const deleteFolderByPrefix = async ({
+  Bucket = process.env.S3_BUCKET_NAME as string,
+  path,
+  Quiet = false,
+}: {
+  Bucket?: string;
+  path: string;
+  Quiet?: boolean;
+}): Promise<DeleteObjectsCommandOutput> => {
+  const fileList = await listDirectoryFiles({
+    path,
+    Bucket,
+  });
+  if (!fileList?.Contents?.length) {
+    throw new BadRequest("empty directories");
+  }
+
+  const urls: string[] = fileList.Contents.map((file) => file.Key as string);
+
+  return await deleteFiles({ urls, Bucket, Quiet });
+};
